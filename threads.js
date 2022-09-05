63b96bbafcc49d0fa9ff5f55ddd8de4fc3368bd5
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2022-09-05 12:06:26 +0200

    example works

--- threads.js
+++ threads.js
@@ -1,9 +1,18 @@
-const { isMainThread, Worker } = require('worker_threads');
+const {
+    isMainThread,
+    workerData,
+    Worker 
+} = require('worker_threads');
 
 if (isMainThread) {
-    console.log(`Main Thread>! Proces ID: ${process.pid}`);
-    new Worker(__filename);
-    new Worker(__filename);
+    console.log(`Main Thread! Proces ID: ${process.pid}`);
+    new Worker(__filename, {
+        workerData: [7, 6, 2, 3]
+    });
+    new Worker(__filename, {
+        workerData: [1, 3, 4, 3]
+    });
 } else {
     console.log(`Worker! Proces ID: ${process.pid}`);
+    console.log(`${workerData} sorted is ${workerData.sort()}`);
 }
